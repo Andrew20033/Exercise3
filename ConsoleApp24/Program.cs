431ee782d80f8335e72e78545f14a6cdf8ba44f2
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 13:12:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string charArray = Console.ReadLine();
+            int i = 0; while (i < charArray.Length) Console.WriteLine(charArray[i++]);
+            int k = charArray.Length - 1; while (k >= 0) Console.Write(charArray[k--]);
+            Console.WriteLine();
+        }
+    }
+}
